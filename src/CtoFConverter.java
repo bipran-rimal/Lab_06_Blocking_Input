84e23025449c969b5d321846b19b194075f48cf1
bipran-rimal <bipran.workspace@gmail.com>
2024-10-07 14:43:40 -0400

    commit

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,2 +1,21 @@
-public class CtoF {
+import java.util.Scanner;
+public class CtoFConverter {
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        boolean done=false;
+        do {
+            System.out.println("Input the temperature in Celsius(C)");
+            if(in.hasNextDouble()){
+                double tempInC = in.nextDouble();
+                double tempInF = 0.00;
+                tempInF = (tempInC * 1.8 + 32);
+                System.out.println(tempInC + " is " + tempInF + " in Fahrenheit");
+                done=true;
+            }
+            else{
+                System.out.println("invalid input");
+                in.nextLine();
+            }
+        }while(!done);
+    }
 }
